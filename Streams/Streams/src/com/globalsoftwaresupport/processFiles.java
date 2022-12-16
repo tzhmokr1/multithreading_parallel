aeebc85d27ebcb8a8f54240acd8e787ab289981a
Kristian Morath <kristian.morath@bluewin.ch>
2022-12-16 12:53:53 +0100

    streams

---
+++ Streams/Streams/src/com/globalsoftwaresupport/processFiles.java
@@ -0,0 +1,20 @@
+package com.globalsoftwaresupport;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class processFiles {
+
+	public static void main(String[] args) throws IOException {
+		
+		// process files with streams
+		String path = "/multithreading_parallel/Streams/Streams/src/com/globalsoftwaresupport/names";
+		Stream<String> nameStream = Files.lines(Paths.get(path));
+		List<String> list = nameStream.collect(Collectors.toList());
+		list.forEach(System.out::println);
+	}
+}
